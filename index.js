d4813efdd9c1f5cf3d2f24fc96358ae6bb36a498
margaritakop <margarita.kopniczky@gmail.com>
2018-11-17 11:47:09 +0000

    start refactor

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 // var Fruit = require('./models/Fruit');
 var Word = require('./models/Word');
-
-
 const https = require('https')
-https.get('https://spreadsheets.google.com/feeds/list/1o9w4hMb1L05AyZjHSrQPZpdDQEQpY75Iaeoy9pQBzd4/od6/public/values?alt=json', (resp) => {
-  let data = '';
 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    spreadsheet = (JSON.parse(data))
-
+let url = 'https://spreadsheets.google.com/feeds/list/1o9w4hMb1L05AyZjHSrQPZpdDQEQpY75Iaeoy9pQBzd4/od6/public/values?alt=json'
+function loadWordsFromOnlineSpreadsheet(url){
     let words = []
-    for (let i = 0; i < spreadsheet.feed.entry.length; i++){
-        word = spreadsheet.feed.entry[i].title.$t
-        correctDef = spreadsheet.feed.entry[i].gsx$correctdefinition.$t
-        incorrectDef = spreadsheet.feed.entry[i].gsx$incorrectdefinition.$t
-        words.push(new Word(word, correctDef, incorrectDef))
-    }
-    console.log(words)
 
-  });
+    https.get(url, (resp) => {
+        let data = '';
+      
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+      
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          spreadsheet = (JSON.parse(data))
+      
+          for (let i = 0; i < spreadsheet.feed.entry.length; i++){
+              word = spreadsheet.feed.entry[i].title.$t
+              correctDef = spreadsheet.feed.entry[i].gsx$correctdefinition.$t
+              incorrectDef = spreadsheet.feed.entry[i].gsx$incorrectdefinition.$t
+              words.push(new Word(word, correctDef, incorrectDef))
+          }
+          console.log(words)
+        })
+      
+      }).on("error", (err) => {
+        console.log("Error: " + err.message)
+      });
+      //return the words!!! callback, sg
+}
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message)
-});
+console.log(loadWordsFromOnlineSpreadsheet(url))
+
 
 //TODO call function to load words array when game is init 
 //lodaSpreadsheet(url) => ('word') => (def: '', 'incorrectDef:'')
