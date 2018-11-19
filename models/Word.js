deb5f87df9d4b36540d8c9e48bdb8330278f2eb2
margaritakop <margarita.kopniczky@gmail.com>
2018-11-19 11:19:28 +0000

    make array of object literals for words and use that

--- models/Word.js
+++
@@ -1,9 +0,0 @@
-class Word {
-    constructor(word, correctDef, incorrectDef) {
-        this.word = word
-        this.correctDef = correctDef
-        this.incorrectDef = incorrectDef
-    }
-}
-
-module.exports = Word
