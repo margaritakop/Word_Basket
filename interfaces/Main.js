84ba090f82777eb03715b08d9a362f056b937439
margaritakop <margarita.kopniczky@gmail.com>
2018-11-19 18:57:38 +0000

    fruit only falling when popup not shown

--- interfaces/Main.js
+++ interfaces/Main.js
@@ -46,26 +46,26 @@ class MainScreen {
         
       
         var speed = 5
-        fruit.y = fruit.y + speed
-        fruit.showFruit()
-
+        if (showPop != true){
+            fruit.y = fruit.y + speed
+            fruit.showFruit()
+            }
        
-        if(635<fruit.y && basket.x - 300 < fruit.x  && fruit.x < basket.x + 300){
-            score++
-            fruit.x = Math.random()*1000
+        if(635 < fruit.y 
+            && basket.x - 300 < fruit.x  
+            && fruit.x < basket.x + 300
+            ){
             fruit.y = 0
+            score++
+            showPop = true
         } 
-        if(635<fruit.y){
-        lives--
-        //TODO fix that only one life decreases
+        if(635 < fruit.y){
+            fruit.y = 0
+            lives --
         }
 
         //Task 3
         //fruits without words?
-
-        if (keyIsDown(UP_ARROW)) {
-            showPop = true;
-        }
     }
     
     heart(x, y) {
