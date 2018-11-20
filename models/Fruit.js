3a111d26f92f3a3b9ce5a1b046ec2c29a6030577
margaritakop <margarita.kopniczky@gmail.com>
2018-11-20 11:21:12 +0000

    Not all the fruit that fall have words

--- models/Fruit.js
+++ models/Fruit.js
@@ -1,14 +1,21 @@
 class Fruit {
     constructor(x, y) {
+        this.word = ''
         this.x = x;
         this.y = y;
     }
     
     pickWord(){
+        console.log(this.word)
         let i = Math.floor(Math.random()*Words.length)
-        this.word = Words[i]
-        //remove the word from the Words array
-        Words.splice(i, 1)
+        //add a chance factor if chosen word is actually assigned
+        if (Math.random() > 0.5){
+            this.word = Words[i]
+            //remove the word from the Words array
+            Words.splice(i, 1)
+        } else {
+            this.word = ''
+        }
     }
 
     showFruit() {
@@ -18,7 +25,9 @@ class Fruit {
         ellipse(this.x + 80, this.y, 10);
         fill('#007EA7');
         textSize(20)
+        if(fruit.word != ''){
         text(fruit.word.word[0], this.x + 40, this.y + 10);
+        }
     }
 }
 
